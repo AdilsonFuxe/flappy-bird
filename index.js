209eff3c348c7ea90962078dcccc1453b682c4d3
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2020-08-29 15:00:55 +0100

    desenhar o flappy bird na tela

---
+++ index.js
@@ -0,0 +1,15 @@
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
+
+contexto.drawImage(
+    sprites,
+    0, 0,
+    33, 24,
+    10, 50,
+    33, 24
+);
