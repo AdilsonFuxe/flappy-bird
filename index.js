f34100806ba46ac908474970b60c7524e1a26688
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2020-08-29 15:02:26 +0100

    fazendo o loop do jogo

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
 
-contexto.drawImage(
-    sprites,
-    0, 0,
-    33, 24,
-    10, 50,
-    33, 24
-);
+
+
+function loop() {
+    contexto.drawImage(
+        sprites,
+        0, 0,
+        33, 24,
+        10, 50,
+        33, 24
+    );
+
+    requestAnimationFrame(loop);
+}
+
+loop();
